9b9a4035ba3484bcd7fbf7774a441c8d122fa5fc
markun4334343 <rakun.mark@gmail.com>
2025-09-10 00:31:59 +0200

    updating application.properties

--- src/main/java/Webtech/Backend/WebsiteApplication.java
+++ src/main/java/Webtech/Backend/WebsiteApplication.java
@@ -1,9 +1,9 @@
-package Webtech;
+package Webtech.Backend;  // This must match!
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "Webtech.Backend")
 public class WebsiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebsiteApplication.class, args);
